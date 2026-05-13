ce83702a290b4133de234b235577b9a0ccf60bee
Daniel Acsinte <13287878+Acsigen@users.noreply.github.com>
2026-05-13 19:48:33 +0200

    Create docker-ubuntu.sh

---
+++ Containers/docker-ubuntu.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1)
+
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker "$USER"
+
+echo "Instalation complete, please logout and login again to run docker commands without sudo."
+
+echo "For rootless mode run:"
+echo "sudo systemctl disable --now docker.service docker.socket"
+echo "sudo rm /var/run/docker.sock"
+echo "dockerd-rootless-setuptool.sh install"
